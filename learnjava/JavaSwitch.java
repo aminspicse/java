f0e547d186ec7aad9d3053762a282653628c9dcd
AL AMIN <aminspicse@gmail.com>
2019-08-25 20:08:35 +0600

    Java Switch

---
+++ learnjava/JavaSwitch.java
@@ -0,0 +1,22 @@
+
+package learnjava;
+
+/**
+ *
+ * @author AL AMIN
+ */
+public class JavaSwitch {
+    public static void main(String arg[]){
+        int day = 20;
+        switch(day){
+            case 19: 
+                System.out.println("This is Good day");
+                break;
+            case 20:
+                System.out.println("Good bur something wrong");
+                break;
+            default:
+                System.out.println("It\'s not good");
+        }
+    }
+}
